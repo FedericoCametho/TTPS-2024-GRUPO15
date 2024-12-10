d92e049ce90b9a7f08a971e50f59fec83249dccc
ignacio.carrera <ignacio.carrera@arba.gov.ar>
2024-12-10 11:45:55 -0300

    eliminacion de comentarios

--- frontend/src/app/comida-create/comida-create.component.ts
+++ frontend/src/app/comida-create/comida-create.component.ts
@@ -27,9 +27,9 @@ export class ComidaCreateComponent {
   ) {
     this.comidaForm = this.fb.group({
       nombre: ['', [Validators.required]],
-      foto: [''], // Campo opcional
+      foto: [''],
       tipoComida: ['', [Validators.required]],
-      inMenu: [false], // Campo opcional
+      inMenu: [false],
       precio: ['', [Validators.required, Validators.min(0)]]
     });
   }
@@ -63,9 +63,9 @@ export class ComidaCreateComponent {
         id: 0, // El id se genera automáticamente en el backend
         nombre: nombre,
         tipoComida: tipoComida,
-        inMenu: inMenu || false, // Valor por defecto si es opcional
+        inMenu: inMenu || false,
         precio: precio,
-        foto: foto || '' // Valor por defecto si es opcional
+        foto: foto || ''
       };
 
       this.comidaService.createComida(comida).subscribe({
